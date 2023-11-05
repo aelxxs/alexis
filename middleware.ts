8cb81aba0e03b8b13b13896aeb5f1c90d0523b89
Alexis Vielma <alexis.vielma.us@gmail.com>
2023-11-04 23:55:39 -0500

    feat: test middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,19 @@
+import { rewrite } from "@vercel/edge";
+
 export const config = {
 	// Only run the middleware on the admin route
 	matcher: "/f",
 };
 
+const file =
+	"https://sf4xxnguvhaohooo.public.blob.vercel-storage.com/articles/blob-HlmeYYVfpatdJoEl2MqEHShBVrdGQl.txt";
+
 export default function middleware(request: Request): Response {
 	const url = new URL(request.url);
 
 	if (url.pathname === "/f") {
-		url.pathname = "/";
+		url.pathname = "/hi";
 	}
-	return Response.redirect(url);
+
+	return rewrite(file);
 }
